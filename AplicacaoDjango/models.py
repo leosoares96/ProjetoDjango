cc1ba23fb68ab6d6ee9460b175ce00d4613a1918
leosoares96 <leo.soares64@outlook.com>
2019-05-12 17:22:16 -0300

    criacao dos modelos ORM e migraco para o BD SQLite

--- AplicacaoDjango/models.py
+++ AplicacaoDjango/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class usuario(models.Model):
+    nome = models.CharField("nome", max_length=50)
+    email = models.EmailField("email", max_length=50)
+    senha = models.CharField("senha", max_length=50)
+
+
+    def __str__(self):
+        return self.nome
+
+class conteudo(models.Model):
+    titulo = models.CharField("titulo", max_length=50)
+    email = models.TextField("texto", max_length=50)
+    senha = models.CharField("autor", max_length=50)
+
+
+    def __str__(self):
+        return self.titulo
+
+class contato(models.Model):
+    nome = models.CharField("nome", max_length=50)
+    assunto = models.CharField("assunto", max_length=50)
+    conteudo = models.TextField("conteudo", max_length=50)
+
+
+    def __str__(self):
+        return self.nome
+
